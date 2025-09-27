a859f3473ff64c421137a6b5c4d5e8b5001d2192
IdentityStolen <shree.kull61@gmail.com>
2025-09-27 13:34:23 -0700

    make updates & server code

---
+++ Makefile
@@ -0,0 +1,4 @@
+gen_from_proto:
+	protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    helloworld/hello.proto
---
+++ greeter_server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/IdentityStolen/HelloGRPC/helloworld"
+	"google.golang.org/grpc"
+)
+
+type myHelloServer struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *myHelloServer) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received %v", req.GetName())
+	return &pb.HelloReply{Message: fmt.Sprintf("Hi %v", req.GetName())}, nil
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":8085")
+	if err != nil {
+		log.Fatalf("Error creating listener: %q", err)
+	}
+	serverRegister := grpc.NewServer()
+	service := &myHelloServer{}
+	pb.RegisterGreeterServer(serverRegister, service)
+	err = serverRegister.Serve(listener)
+	if err != nil {
+		log.Fatalf("Something went wrong: %q", err)
+	}
+}
